f7a10b25ff6d3e9cc6b385d254130103fc406733
Vinicius Pittigliani Perego <v_perego18@cs.uni-kl.de>
2018-12-21 17:04:12 +0100

    run & build refactoring

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -4,6 +4,7 @@ if [ "$#" = 0 ]; then
     echo "Usage $0 [cluster] app-name config-file"
     exit 1
 fi
+rm -fr resources/output/
 
 sbt package
 mkdir /tmp/spark-events
@@ -14,6 +15,7 @@ rm -fr resources/stream_output
 
 #SPARK=/home/vinicius/spark-2.4.0-bin-hadoop2.6/bin/spark-submit
 SPARK=spark-submit
+PACKAGES=org.apache.spark:spark-streaming-kafka-0-10_2.11:2.3.0,org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0
 TARGET=target/scala-2.11/joinexperiments_2.11-0.1.jar
 
 
@@ -30,7 +32,7 @@ if [ "$1" = "cluster" ]; then
 #    spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.2 --master yarn --deploy-mode cluster target/scala-2.11/joinexperiments_2.11-0.1.jar "$2" $RES_LOCATION
 else
     RES_LOCATION="file:///home/vinicius/IdeaProjects/joinExperiments/resources/config/""$2"
-    $SPARK --packages org.apache.spark:spark-streaming-kafka-0-10_2.11:2.3.0 --master local[*] \
+    $SPARK --packages $PACKAGES --master local[*] \
     --executor-memory 2g \
     --driver-memory 2g \
     $TARGET "$1" $RES_LOCATION
