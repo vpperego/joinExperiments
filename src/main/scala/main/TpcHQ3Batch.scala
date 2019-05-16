155e8855c7a487afeb6c2a5c9fb1a25961385687
vpperego <vinicius.pittigliani@gmail.com>
2019-05-16 13:13:40 -0300

    Q3 batch update

--- src/main/scala/main/TpcHQ3Batch.scala
+++ src/main/scala/main/TpcHQ3Batch.scala
@@ -2,7 +2,7 @@ package main
 
 import java.sql.Date
 
-import main.startup.spark
+import main.startup.{config, spark}
 
 case class Customer(custKey: Int, mktSegment: String)
 case class Order(orderKey: Int,custKey: Int, orderDate: Date, shipPriority: Int)
@@ -16,28 +16,26 @@ object TpcHQ3Batch {
   println("Starting TpcHQ3Batch")
   var sc = spark.sparkContext
   var customer  = spark.read.format("csv")
-    .load("file:///home/vinicius/IdeaProjects/joinExperiments/resources/tpchQ3/customer.tbl").as[(String)]
+    .load(config("customerPath")).as[(String)]
     .map{line =>
       var fields = line.split('|')
       Customer(fields(0).toInt,fields(6))
     }
-    .filter{cust => cust.mktSegment == "BUILDING"  }
 
 
-  var order  =   spark.read.format("csv").load("file:///home/vinicius/IdeaProjects/joinExperiments/resources/tpchQ3/order.tbl").as[(String)]
+
+  var order  =   spark.read.format("csv").load(config("ordersPath")).as[(String)]
     .map{line =>
       var fields = line.split('|')
       Order(fields(0).toInt, fields(1).toInt,
         Date.valueOf(fields(4)), fields(7).toInt)
     }
-    .filter(order => order.orderDate.before(Date.valueOf("1995-03-15")))
 
-  var lineItem  = spark.read.format("csv").load("file:///home/vinicius/IdeaProjects/joinExperiments/resources/tpchQ3/lineitem.tbl").as[(String)]
+  var lineItem  = spark.read.format("csv").load(config("lineItemPath")).as[(String)]
     .map{line =>
       var fields = line.split('|')
       LineItem(fields(0).toInt, fields(5).toDouble * (1 - fields(6).toDouble),Date.valueOf(fields(10)))
     }
-    .filter(line => line.shipDate.after(Date.valueOf("1995-03-15")))
 
 
   var p = customer
@@ -46,9 +44,4 @@ object TpcHQ3Batch {
       .count()
 
     println("Result size: " + p)
-
-  //  println("Result size: " + order.count())
-//  println("Result size: " + lineItem.count())
-
-
 }
